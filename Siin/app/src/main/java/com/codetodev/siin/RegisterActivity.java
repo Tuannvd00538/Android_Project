dd700a5f25af0dc338c5d9de4c21f9a46e512733
Ngô Văn Tuấn <tuannvd00538@fpt.edu.vn>
2018-05-24 23:51:51 +0700

    App Chat

--- Siin/app/src/main/java/com/codetodev/siin/RegisterActivity.java
+++ Siin/app/src/main/java/com/codetodev/siin/RegisterActivity.java
@@ -5,8 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.UnsupportedEncodingException;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    ServerConnect sc = new ServerConnect();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,10 +21,21 @@ public class RegisterActivity extends AppCompatActivity {
 
         //Action click btn register
         final Button btnRegister = findViewById(R.id.RegisterBtn);
+        final TextView usernameRegister = findViewById(R.id.usernameRegister);
+        final TextView passwordRegister = findViewById(R.id.passwordRegister);
+        final TextView emailRegister = findViewById(R.id.emailRegister);
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(RegisterActivity.class.getSimpleName(), "CLick!");
+                if (usernameRegister.getText().length() == 0 || passwordRegister.getText().length() == 0 || emailRegister.getText().length() == 0) {
+                    Log.d(LoginActivity.class.getSimpleName(), "NullPointerException");
+                } else {
+                    try {
+                        sc.register(usernameRegister.getText().toString(), passwordRegister.getText().toString(), emailRegister.getText().toString());
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
     }
--- Siin/app/src/main/java/com/codetodev/siin/ServerConnect.java
+++ Siin/app/src/main/java/com/codetodev/siin/ServerConnect.java
@@ -8,8 +8,8 @@ import org.json.JSONObject;
 
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
@@ -18,50 +18,37 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.net.URLEncoder;
 
 public class ServerConnect {
 
     String urlServer = "http://android-siin.appspot.com";
 
-    public void login(String username, String password) throws UnsupportedEncodingException {
+    public boolean login(String username, String password) throws UnsupportedEncodingException {
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
                 .permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        StringBuilder text = null;
-
         try {
-
+            URL loginURL = new URL(urlServer + "/_api/v1/authentication");
+            URLConnection conn = loginURL.openConnection();
+            conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            conn.setDoOutput(true);
+            OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
             JSONObject dataObject = new JSONObject();
             dataObject.put("username", username);
             dataObject.put("password", password);
             String data = dataObject.toString();
+            osw.write(data);
+            osw.flush();
 
-            //Create connection and send data login to server
-            URL loginUrl = new URL(urlServer + "/_api/v1/authentication");
-            HttpURLConnection conn = (HttpURLConnection) loginUrl.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Content-Type", "application/json");
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-            conn.connect();
-
-            OutputStream os = new BufferedOutputStream(conn.getOutputStream());
-            os.write(data.getBytes());
-            os.flush();
-
-            //Get server response data
-            String line;
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
             while ((line = br.readLine()) != null) {
-                text.append(line).append("\n");
+                System.out.println(line + "\n");
             }
-
+            osw.close();
             br.close();
-            conn.disconnect();
-
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -69,12 +56,42 @@ public class ServerConnect {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+        return false;
+    }
 
-        if (!text.toString().isEmpty()) {
-            //Show response
-            Log.d(ServerConnect.class.getSimpleName(), text.toString());
-        } else {
-            Log.d(ServerConnect.class.getSimpleName(), "False!");
+    public void register(String username, String password, String email) throws UnsupportedEncodingException {
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        try {
+            URL loginURL = new URL(urlServer + "/_api/v1/member");
+            URLConnection conn = loginURL.openConnection();
+            conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            conn.setDoOutput(true);
+            OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
+            JSONObject dataObject = new JSONObject();
+            dataObject.put("username", username);
+            dataObject.put("password", password);
+            dataObject.put("email", email);
+            String data = dataObject.toString();
+            osw.write(data);
+            osw.flush();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line + "\n");
+            }
+            osw.close();
+            br.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
     }
 }
